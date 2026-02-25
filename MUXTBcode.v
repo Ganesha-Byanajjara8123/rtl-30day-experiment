c6ed1b200f36f2aa6ca970cdf955be478ea5804d
Ganesha-Byanajjara8123 <byanajjaraganesha@gmail.com>
2026-02-25 21:23:01 +0530

    Update MUXTBcode.v

--- MUXTBcode.v
+++ MUXTBcode.v
@@ -1,11 +1,11 @@
 // 4:1 MUX tb code
 module MUX_tb;
-  input reg   [3:0]   A, B, C, D;
+  input reg   [2:0]   A, B, C, D;
   input reg   [1:0]   sel;
-  output wire [3:0]   X;
+  output wire [2:0]   X;
 
   MUX uut (
-    .A(A), .B(B), .C(C), .D(D), sel(sel), X(x)
+    .A(A), .B(B), .C(C), .D(D), .sel(sel), .X(X)
   );
   
 initial begin
@@ -14,7 +14,7 @@ A=2'b01; sel=2'b01; #10;
 A=2'b10; sel=2'b10; #10;
 A=2'b11; sel=2'b11; #10;
   #100 $finish;
-  $monitor("t=%0 | A=%d | B=%d | C=%d | D=%d | sel=%d | X=%d", $time A, B, C, D, sel, X};
+  $monitor("t=%0t | A=%d | B=%d | C=%d | D=%d | sel=%d | X=%d", $time A, B, C, D, sel, X};
 end
 endmodule
 
