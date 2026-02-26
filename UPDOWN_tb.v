ab41e938b4d284ad78d17e4abbaf259069e1565f
Ganesha-Byanajjara8123 <byanajjaraganesha@gmail.com>
2026-02-26 20:33:17 +0530

    Update UPDOWN_tb.v

--- UPDOWN_tb.v
+++ UPDOWN_tb.v
@@ -3,12 +3,14 @@
 module tb_updown_counter();
     reg clk;
     reg reset;
+    reg en;
     reg up_down;
     wire [7:0] count;
 
     updown_counter_8bit uut (
         .clk(clk),
         .reset(reset),
+        .en(en),
         .up_down(up_down),
         .count(count)
     );
@@ -19,18 +21,20 @@ module tb_updown_counter();
     end
 
     initial begin
-        reset = 1'b1; 
+        reset = 1'b1;
+        en = 1'b0;
         up_down = 1'b0;
 
         #10;
         reset = 1'b0; 
-        
+        en = 1'b1;
         up_down = 1'b1;
         #200; 
         up_down = 1'b0;
        #50;
         reset = 1'b1;
         #10;
+        en = 1'b0;
         reset = 1'b0; 
         #50;
 
@@ -38,5 +42,7 @@ module tb_updown_counter();
         $finish; 
     end
 
+    initial begin
+      $monitor("Time=%0t, Reset=%0b, en=%0b, Up/Down=%0b, Count=%0d", $time, reset, en, up_down, count);  
+    end
 endmodule
-
