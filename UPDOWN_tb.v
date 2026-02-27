a301adca73d33f7fa5bebe38449a0263d440f1d9
Ganesha-Byanajjara8123 <byanajjaraganesha@gmail.com>
2026-02-27 21:58:04 +0530

    Update UPDOWN_tb.v

--- UPDOWN_tb.v
+++ UPDOWN_tb.v
@@ -1,19 +1,24 @@
 // Code your testbench here
 // or browse Examples
-module tb_updown_counter();
+// Code your testbench here
+// or browse Examples
+module updown_counter_tb();
+    localparam WIDTH = 4;
     logic clk;
     logic reset;
     logic en;
     logic up_down;
     logic [WIDTH-1:0] count;
     logic tc;
+ 
 
-    updown_counter_8bit uut (
+    updown_counter uut (
         .clk(clk),
         .reset(reset),
         .en(en),
         .up_down(up_down),
-        .count(count)
+        .count(count),
+        .tc(tc)
     );
 
     initial begin
@@ -22,18 +27,21 @@ module tb_updown_counter();
     end
 
     initial begin
-        reset = 1'b1;
-        en = 1'b0;
-        up_down = 1'b0;
-        #15 rst = 1'b1;
+       reset = 1'b1; #15;
+           
+      reset = 1'b0;
+        en = 1'b1;
+        up_down = 1'b1;
+      
+     
         @(posedge clk)
 
         up_down = 1'b1;
-        repeate@(20) @(posedge clk);
-
+      repeat(20) @(posedge clk);
+      
         up_down = 1'b0;
-        repeate@(20) @(posedge clk);
-        
+      repeat(20) @(posedge clk); 
+      
 
         $display("Simulation finished");
         $finish; 
