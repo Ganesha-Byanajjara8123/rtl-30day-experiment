8d57fc13e97f39d1879493056fe64297f2746ccd
Ganesha-Byanajjara8123 <byanajjaraganesha@gmail.com>
2026-02-27 21:10:08 +0530

    Update UPDOWN_counter.v

--- UPDOWN_counter.v
+++ UPDOWN_counter.v
@@ -3,8 +3,9 @@ module updown_counter_8bit #(parameter WIDTH = 8)(
     logic clk,
     logic reset,
     logic en, 
-    logic up_down, 
-    logic [WIDTH-1:0] count
+    logic up_down,
+    logic [WIDTH-1:0] count,
+    logic tc       //tc (terminal count)
 );
 
   always_ff @(posedge clk)begin
@@ -25,5 +26,6 @@ module updown_counter_8bit #(parameter WIDTH = 8)(
     end
 end
 end
+    assign tc = (count == 8'd15);
 
 endmodule
