b8b94d0739a807115a53dd76d27fd435e70bc72e
Ganesha-Byanajjara8123 <byanajjaraganesha@gmail.com>
2026-03-01 20:08:17 +0530

    Update FIFO_tb.sv

--- FIFO_tb.sv
+++ FIFO_tb.sv
@@ -164,6 +164,21 @@ module fifo_syn_tb();
          $time, uut.wr_ptr, uut.rd_ptr, full, empty, rdata);
   end
 endmodule
+
+
+
+## and this is the o/p of the above day5 code
+
+# KERNEL: ASDB file was created in location /home/runner/dataset.asdb
+# KERNEL: t=0 wr_ptr=x rd_ptr=x full=x empty=x rdata=x
+# KERNEL: t=15 wr_ptr=0 rd_ptr=0 full=0 empty=1 rdata=0
+# KERNEL: t=75 wr_ptr=1 rd_ptr=0 full=0 empty=0 rdata=0
+# KERNEL: t=105 wr_ptr=2 rd_ptr=0 full=0 empty=0 rdata=0
+# KERNEL: t=135 wr_ptr=3 rd_ptr=0 full=1 empty=0 rdata=0
+# KERNEL: t=195 wr_ptr=3 rd_ptr=1 full=0 empty=0 rdata=10
+# KERNEL: t=225 wr_ptr=3 rd_ptr=2 full=0 empty=0 rdata=20
+# KERNEL: t=255 wr_ptr=3 rd_ptr=3 full=0 empty=1 rdata=30
+# RUNTIME: Info: RUNTIME_0068 testbench.sv (82): $finish called.
     
 
     
